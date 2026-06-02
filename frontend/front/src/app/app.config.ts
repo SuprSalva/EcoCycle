8cda7e673423bf62b5dd1c04fc9d0e945de9a905
ZavalaUriel <zavalaurieloficial@gmail.com>
2026-06-02 10:09:13 -0600

    feat: integrate Firebase and Firestore configuration into the Angular application

--- frontend/front/src/app/app.config.ts
+++ frontend/front/src/app/app.config.ts
@@ -2,11 +2,6 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-<<<<<<< HEAD
-
-export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
-=======
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
@@ -18,5 +13,4 @@ export const appConfig: ApplicationConfig = {
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideFirestore(() => getFirestore())
   ]
->>>>>>> e9baa1e4074ac01438e712f11fa2184c27ce8485
 };
